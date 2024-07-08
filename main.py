f5513cadbff612f3f33b258692d7ef86e87c492f
jcnwkoya <jcnw@pop06.odn.ne.jp>
2024-07-08 15:23:28 +0900

    17thcommit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
+import os
 # import openpyxl
-
+file_path3 = os.path.join(os.getcwd(), 'AImessage.xlsx')
 # 簡単なExcelデータの表示
 st.title('Sample Streamlit App')
 df = pd.DataFrame({
@@ -9,3 +10,5 @@ df = pd.DataFrame({
     'Column2': [4, 5, 6]
 })
 st.write(df)
+df3 = pd.read_excel(file_path3,sheet_name='AImessage')
+st.table(df3)
